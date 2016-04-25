f77a9e6bfc55b602cbbd991c4f9b5507e0f1453f
berziiii <brian@offpistedesigns.com>
2016-04-25 16:19:31 -0400

    update of methods and remove instance variables

--- script.rb
+++ script.rb
@@ -1,113 +1,76 @@
 require 'date'
 
-@old_file = 'WDI_BOS_011.ics'
+old_file = 'WDI_BOS_011.ics'
 
-@new_file = 'WDI_BOS_012.ics'
-
-#start date of old cohort
-@old_calendar_start_date = Date.new(2016,03,21)
+new_file = 'WDI_BOS_012.ics'
 
 # #n = number of days between cohorts (7 days in a week and 4 weeks)
-@n = 56
+NUM_DAYS = 56
 
-@old_calendar = File.open(@old_file)
+old_calendar = File.open(old_file)
 
-@new_calendar = File.open(@new_file, "w")
+new_calendar = File.open(new_file, "w")
 
-### DTSTART time value change ###
+def split_two(line)
 
-def split_two
-  @d = line.split(/\:([0-9]{8})/, 2)
-  @header = @d[0]
-  @d = Date.parse(@d[1])
+  d = line.split(/\:([0-9]{8})/, 2)
+  header = d[0]
+  d = Date.parse(d[1])
+  d = (d + NUM_DAYS).to_s.gsub('-', '')
+  d = header + ':' + d
+  line = d
 
-  # check to see if date is before the date of cohort, if so, do not change (Daylight savings, etc.)
-  if @d < @old_calendar_start_date
-
-    @d = @d.to_s.gsub('-', '')
-
-    line = @header + ':' + @d
-
-  else
-
-    @d = (@d+@n).to_s.gsub('-', '')
-
-    line = @header + ':' + @d
-
-  end
 end
 
-def split_three
-  @d = line.split(/\:([0-9]{8})/, 3)
-  @header = @d[0]
-  @tail = @d[2]
-  @d = Date.parse(@d[1])
+def split_three (line)
 
-  # check to see if date is before the date of cohort, if so, do not change (Daylight savings, etc.)
-  if @d < @old_calendar_start_date
+  d = line.split(/\:([0-9]{8})/, 3)
+  header = d[0]
+  tail = d[2]
+  d = Date.parse(d[1])
+  d = (d + NUM_DAYS).to_s.gsub('-', '')
+  d = header + ':' + d + tail
+  line = d
 
-    @d = @d.to_s.gsub('-', '')
-
-    line = @header + ':' + @d + @tail
-
-  else
-
-    @d = (@d+@n).to_s.gsub('-', '')
-
-    line = @header + ':' + @d + @tail
-
-  end
 end
 
 
-@old_calendar.map do |line|
+old_calendar.map do |line|
 
-  if line.include?("DTSTART:")
+  if line.include?("RRULE:") && line.include?("UNTIL=")
 
-    line.method(:split_three)
+    d = line.split(/\=([0-9]{8})/, 3)
+    header = d[0]
+    tail = d[2]
+    d = Date.parse(d[1])
+    d = (d + NUM_DAYS).to_s.gsub('-', '')
+    d = header + '=' + d + tail
+    line = d
+
+  elsif line.include?("DTSTART:")
+
+    line = split_three(line)
 
   elsif line.include?("DTEND:")
 
-    line.method(:split_three)
+    line = split_three(line)
 
   elsif line.include?("DTSTART;VALUE=")
 
-    line.method(:split_two)
+    line = split_two(line)
 
   elsif line.include?("DTEND;VALUE=")
 
-    line.method(:split_two)
+    line = split_two(line)
 
   elsif line.include?("DTSTART;TZID=")
 
-    line.method(:split_three)
-
-  elsif line.include?("RRULE:") && line.include?("UNTIL=")
-
-    @d = line.split(/\=([0-9]{8})/, 3)
-    @header = @d[0]
-    @tail = @d[2]
-    @d = Date.parse(@d[1])
-
-    # check to see if date is before the date of cohort, if so, do not change (Daylight savings, etc.)
-    if @d < @old_calendar_start_date
-
-      @d = @d.to_s.gsub('-', '')
-
-      line = @header + '=' + @d + @tail
-
-    else
-
-      @d = (@d+@n).to_s.gsub('-', '')
-
-      line = @header + '=' + @d + @tail
-
-    end
+    line = split_three(line)
 
   end
 
-  @new_calendar.write(line)
+  new_calendar.write(line)
 
 end
 
-@new_calendar.close
+new_calendar.close
--- script_1.rb
+++ script_1.rb
@@ -4,108 +4,109 @@ require 'date'
 
 @new_file = 'WDI_BOS_012.ics'
 
-#start date of old cohort
-@old_calendar_start_date = Date.new(2016,03,21)
-
-# #n = number of days between cohorts (7 days in a week and 4 weeks)
+# #n = number of days between cohorts (7 days in a week and 8 weeks)
 @n = 56
 
 @old_calendar = File.open(@old_file)
 
 @new_calendar = File.open(@new_file, "w")
 
-### DTSTART time value change ###
+# def split_two
 
-def split_two
-  @d = line.split(/\:([0-9]{8})/, 2)
-  @header = @d[0]
-  @d = Date.parse(@d[1])
+#   @line = line
+#   @d = line.split(/\:([0-9]{8})/, 2)
+#   @header = @d[0]
+#   @d = Date.parse(@d[1])
+#   @d = (@d+@n).to_s.gsub('-', '')
+#   @d = @header + ':' + @d
+#   line = @d
+#
+# end
+#
+# def split_three
 
-  # check to see if date is before the date of cohort, if so, do not change (Daylight savings, etc.)
-  if @d < @old_calendar_start_date
-
-    @d = @d.to_s.gsub('-', '')
-
-    line = @header + ':' + @d
-
-  else
-
-    @d = (@d+@n).to_s.gsub('-', '')
-
-    line = @header + ':' + @d
-
-  end
-end
-
-def split_three
-  @d = line.split(/\:([0-9]{8})/, 3)
-  @header = @d[0]
-  @tail = @d[2]
-  @d = Date.parse(@d[1])
-
-  # check to see if date is before the date of cohort, if so, do not change (Daylight savings, etc.)
-  if @d < @old_calendar_start_date
-
-    @d = @d.to_s.gsub('-', '')
-
-    line = @header + ':' + @d + @tail
-
-  else
-
-    @d = (@d+@n).to_s.gsub('-', '')
-
-    line = @header + ':' + @d + @tail
-
-  end
-end
-
+#   @line = line
+#   @d = line.split(/\:([0-9]{8})/, 3)
+#   @header = @d[0]
+#   @tail = @d[2]
+#   @d = Date.parse(@d[1])
+#   @d = (@d+@n).to_s.gsub('-', '')
+#   @d = @header + ':' + @d + @tail
+#   line = @d
+#
+# end
 
 @old_calendar.map do |line|
 
-  if line.include?("DTSTART:")
-
-    line.method(:split_three)
-
-  elsif line.include?("DTEND:")
-
-    line.method(:split_three)
-
-  elsif line.include?("DTSTART;VALUE=")
-
-    line.method(:split_two)
-
-  elsif line.include?("DTEND;VALUE=")
-
-    line.method(:split_two)
-
-  elsif line.include?("DTSTART;TZID=")
-
-    line.method(:split_three)
-
-  elsif line.include?("RRULE:") && line.include?("UNTIL=")
+  if line.include?("RRULE:") && line.include?("UNTIL=")
 
     @d = line.split(/\=([0-9]{8})/, 3)
     @header = @d[0]
     @tail = @d[2]
     @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + '=' + @d + @tail
+    line = @d
 
-    # check to see if date is before the date of cohort, if so, do not change (Daylight savings, etc.)
-    if @d < @old_calendar_start_date
+  elsif line.include?("DTSTART:")
 
-      @d = @d.to_s.gsub('-', '')
+    @d = line.split(/\:([0-9]{8})/, 3)
+    @header = @d[0]
+    @tail = @d[2]
+    @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + ':' + @d + @tail
+    line = @d
 
-      line = @header + '=' + @d + @tail
+  elsif line.include?("DTEND:")
 
-    else
+    @d = line.split(/\:([0-9]{8})/, 3)
+    @header = @d[0]
+    @tail = @d[2]
+    @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + ':' + @d + @tail
+    line = @d
 
-      @d = (@d+@n).to_s.gsub('-', '')
+  elsif line.include?("DTSTART;VALUE=")
 
-      line = @header + '=' + @d + @tail
+    @d = line.split(/\:([0-9]{8})/, 2)
+    @header = @d[0]
+    @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + ':' + @d
+    line = @d
 
-    end
+  elsif line.include?("DTEND;VALUE=")
+
+    @d = line.split(/\:([0-9]{8})/, 2)
+    @header = @d[0]
+    @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + ':' + @d
+    line = @d
+
+  elsif line.include?("DTSTART;TZID=")
+
+    @d = line.split(/\:([0-9]{8})/, 3)
+    @header = @d[0]
+    @tail = @d[2]
+    @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + ':' + @d + @tail
+    line = @d
+
+  elsif line.include?("DTEND;TZID=")
+
+    @d = line.split(/\:([0-9]{8})/, 3)
+    @header = @d[0]
+    @tail = @d[2]
+    @d = Date.parse(@d[1])
+    @d = (@d+@n).to_s.gsub('-', '')
+    @d = @header + ':' + @d + @tail
+    line = @d
 
   end
-
   @new_calendar.write(line)
 
 end
